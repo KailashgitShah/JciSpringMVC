2c5bbd679d85405c64e27fcb5f44479ff904a92d
vishal.vishwakarma <vishal.vishwakarma@.cyfuture.com>
2024-11-28 01:31:49 +0530

    Code Deployed on production server for inventory query changes

--- src/com/jci/service/Impl/SendMail.java
+++ src/com/jci/service/Impl/SendMail.java
@@ -123,17 +123,16 @@ public class SendMail {
 		}
 	}
 
-	public void sendEmailforPayment(InternetAddress[] toAddresses, String body, String subject, String filePath,
-			String fileName) {
-
+	public void sendEmailforPayment(InternetAddress[] toAddresses, String body, String subject, String filename,
+			String username1) {
+       // System.out.println("send mail data = "+body+subject+filename+username1);
+		String smtpHost = "smtp.gmail.com";
 		int smtpPort = 587; // Use 587 for TLS or 465 for SSL
-		
-		
-//		// for official use
-		String smtpHost = "smtp.office365.com";
-		String username = "jci.erp.ops@jcimail.in"; // Office 365 email address
-		String password = "Jute@1234";
-	
+		// String username = "jci.sahyog@jcimail.in"; // Office 365 email address
+		// String password = "Patsan@2024";
+		String username = "jci.erp@jcimail.in"; // Office 365 email address
+		String password = "Patsan@2024";
+		// String recipient = "vishal.vishwakarma@cyfuture.com"; // Recipient's email
 		// address
 
 		Properties props = new Properties();
@@ -141,30 +140,32 @@ public class SendMail {
 		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.smtp.host", smtpHost);
 		props.put("mail.smtp.port", smtpPort);
-		props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+	    props.put("mail.smtp.ssl.protocols","TLSv1.2");	
 
 		Session session = Session.getInstance(props);
 
 		try {
-			Thread.currentThread().setContextClassLoader( getClass().getClassLoader() );
+
 			Message message = new MimeMessage(session);
 			message.setRecipients(Message.RecipientType.TO, toAddresses);
 			message.setFrom(new InternetAddress(username));
 			message.setSubject(subject);
+			message.setText(body);
 
-			BodyPart messageBodyPart = new MimeBodyPart();
-			messageBodyPart.setText(body);
-
-			Multipart multipart = new MimeMultipart();
-			multipart.addBodyPart(messageBodyPart);
-
-			messageBodyPart = new MimeBodyPart();
-
-			DataSource source = new FileDataSource(filePath);
-			messageBodyPart.setDataHandler(new DataHandler(source));
-			messageBodyPart.setFileName(fileName);
-			multipart.addBodyPart(messageBodyPart);
-			message.setContent(multipart);
+			
+			 BodyPart messageBodyPart = new MimeBodyPart(); messageBodyPart.setText(body);
+			 
+			 
+			 Multipart multipart = new MimeMultipart();
+			 multipart.addBodyPart(messageBodyPart);
+			 
+			 messageBodyPart = new MimeBodyPart();
+			 
+			 DataSource source = new FileDataSource(filename);
+			 messageBodyPart.setDataHandler(new DataHandler(source));
+			 messageBodyPart.setFileName(username1);
+			 multipart.addBodyPart(messageBodyPart); message.setContent(multipart);
+			 
 			Transport transport = session.getTransport("smtp");
 			transport.connect(smtpHost, smtpPort, username, password);
 			transport.sendMessage(message, message.getAllRecipients());
@@ -178,5 +179,7 @@ public class SendMail {
 		}
 
 	}
+
+
 
 }
